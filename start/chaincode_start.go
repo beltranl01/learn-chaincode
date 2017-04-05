be07a997717371e1c3f22e203b30678fbaa0d4a6
Neil Beltran <Neil Beltran>
2017-04-06 01:03:07 +0800

    Copiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/* This is a sample edit to check if the changes wil lbe noticed */
+
 package main
 
 import (
